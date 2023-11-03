d117159c31fee2162a742b6bda8c5691e81ceec7
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2023-11-03 09:01:39 -0500

    Semana 14

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,13 @@ const submitBtn = document.getElementById('submitBtn');
 const userCardContainer = document.getElementById('userCardContainer');
 
 async function getUsers(){
-    let response = await fetch('http://localhost:8080/user/all');
+    let response = await fetch('http://localhost:8080/user/all',{
+        method: 'GET',
+        headers: {
+            'Authorization':'0de79f93-6dd9-4cd1-88d7-09cc04f2811e'
+        },
+        body: data
+    });
     let json = await response.json();
     console.log(json);
 
